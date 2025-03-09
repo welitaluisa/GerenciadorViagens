732d4909f73f4878dd11168eda3dba8ee21e796b
welitaluisa <welitaluisa@gmail.com>
2025-03-09 00:04:15 -0300

    add teste de contrato

--- src/test/java/com/montanha/isolada/ViagensTest.java
+++ src/test/java/com/montanha/isolada/ViagensTest.java
@@ -7,11 +7,10 @@ import com.montanha.factory.ViagemDataFactory;
 import com.montanha.pojo.Usuario;
 import com.montanha.pojo.Viagem;
 import io.restassured.http.ContentType;
-
 import java.io.IOException;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class ViagensTest {
     private String token;
@@ -24,35 +23,67 @@ public class ViagensTest {
         basePath = "/api";
 
         Usuario usuarioAdmin = UsuarioDataFactory.criarUsuarioAdministrador();
-        
+
 
         // Realiza a solicitação HTTP
         this.token = given()
-            .contentType("application/json")
-            .body(usuarioAdmin)
+                .contentType("application/json")
+                .body(usuarioAdmin)
         .when()
-            .post("/v1/auth")
+                .post("/v1/auth")
         .then()
-            .extract().path("data.token"); // Extrai o token do corpo da resposta
+                .extract().path("data.token"); // Extrai o token do corpo da resposta
     }
-    
+
     @Test
     public void testCadastroDeViagemValidaRetornaSucesso() throws IOException {
-    
-    Viagem viagemValida = ViagemDataFactory.criarViagemValida();
 
-    given()
-        .contentType(ContentType.JSON)
-        .body(viagemValida)
-        .header("Authorization", token)
-    .when()
-        .post("/v1/viagens")
-    .then()
-        .assertThat()
-            .statusCode(201)
-            .body("data.localDeDestino", equalTo("Salvador"))
-            .body("data.acompanhante", equalToIgnoringCase("welita"));
+        Viagem viagemValida = ViagemDataFactory.criarViagemValida();
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(viagemValida)
+                .header("Authorization", token)
+        .when()
+                .post("/v1/viagens")
+        .then()
+                .assertThat()
+                .statusCode(201)
+                .body("data.localDeDestino", equalTo("Salvador"))
+                .body("data.acompanhante", equalToIgnoringCase("welita"));
 
     }
 
+    @Test
+    public void testCadastroDeViagemValidaContrato() throws IOException {
+
+        // configurações Rest-Assured
+        baseURI = "http://localhost";
+        port = 8089;
+        basePath = "/api";
+
+        Usuario usuarioAdmin = UsuarioDataFactory.criarUsuarioAdministrador();
+
+        String token = given()
+                .contentType(ContentType.JSON)
+                .body(usuarioAdmin)
+        .when()
+                .post("/v1/auth")
+        .then()
+                .extract()
+                .path("data.token");
+
+        Viagem viagemValida = ViagemDataFactory.criarViagemValida();
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(viagemValida)
+                .header("Authorization", token)
+        .when()
+                .post("/v1/viagens")
+        .then()
+                .assertThat()
+                .statusCode(201)
+                .body(matchesJsonSchemaInClasspath("schemas/postV1ViagensValida.json"));
+    }
 }
